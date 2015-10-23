abf8859c547e4f3c1f53b2b8a4214b49962fe2e7
mdb-webdev <vm187sharing@gmail.com>
2015-10-23 18:03:18 -0500

    First commit - after master i guess
    created and added readme file.
    
    created and added some script.sh

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Readme.md  
+
+My first repo.
+
+Made entirely on my own.
+
+  - hi
+  - blah
+  - yada  
+
+### I like Seinfeld ###
+but i think i may have misplelled that.
+
+
+---
---
+++ some-script.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# An example shell script to commit
+#
+# Lorem ipsum dolor blah blah yada.
+# Ignore the following: appropriate message if it
+# wants to stop the commit.  The hook is
+# allowed to edit the commit message file.
+#
+# Thank you. sincerely, someone.
+
+echo " "
+echo "-----------------------------------------------------------"
+echo "hello"
+echo "-----------------------------------------------------------"
+echo " "
+echo "           ~ LISTING ALL FILES AND DIRECTORIES: ~"
+echo " "
+echo "-----------------------------------------------------------"
+echo " "
+echo " "
+echo " "
+cd
+pwd
+echo "--------------------"
+echo " "
+ls *
+echo " "
+echo " "
+echo "-----------------------------------------------------------"
+date
+echo "           ~ END OF SCRIPT ~ "
+echo "-----------------------------------------------------------"
